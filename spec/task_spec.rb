385edf8c690bdad7d7d170a69fe81dc532150de6
Jennifer <yortree@gmail.com>
2015-05-12 07:28:48 -0700

    Update list method and specs.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,37 +1,52 @@
 require('rspec')
-require('task')
+require('pg')
+require('list')
+require('spec_helper')
+
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM lists *;")
+  end
+end
+
 
 describe(Task) do
-  before() do
-    Task.clear()
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it('adds a task to the array of saved tasks') do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
   end
 
   describe("#description") do
-    it("lets you give it a description") do
-      test_task = Task.new("scrub the zebra")
-      expect(test_task.description()).to(eq("scrub the zebra"))
+    it('lets you read the description out') do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.description()).to(eq("learn SQL"))
     end
   end
-end
 
-describe(".all") do
-  it("is empty at first") do
-    expect(Task.all()).to(eq([]))
+  describe("#list_id") do
+    it('lets you read the list ID out') do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
   end
-end
 
-describe("#save") do
-  it("adds a task to the array of saved tasks") do
-    test_task = Task.new("wash the lion")
-    test_task.save()
-    expect(Task.all()).to(eq([test_task]))
+  describe("#==") do
+    it("is the same task if it ihas the sasme description and list ID") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(task1).to(eq(task2))
+    end
   end
-end
 
-describe(".clear") do
-  it("empties out all of the saved tasks") do
-    Task.new("wash the lion").save()
-    Task.clear()
-    expect(Task.all()).to(eq([]))
-  end
 end
